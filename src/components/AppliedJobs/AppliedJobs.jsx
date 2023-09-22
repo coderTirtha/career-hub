c9ff1a09e03a37a4a55dbeaffab9aa756eb247c9
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2023-09-22 20:04:38 +0600

    applied job filters added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -7,11 +7,25 @@ import { BsChevronDown } from "react-icons/bs";
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayFilteredJobs, setDisplayFilteredJobs] = useState([]);
+
+    const handleJobFilter = filter => {
+        if(filter === 'all') {
+            setDisplayFilteredJobs(appliedJobs);
+        } else if (filter === "remote") {
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === "Remote");
+            setDisplayFilteredJobs(remoteJobs);
+        } else if (filter === "onsite") {
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === "Onsite");
+            setDisplayFilteredJobs(onsiteJobs);
+        }
+    }
     useEffect(() => {
         const storedJobIds = getStoredJobApplications();
         if (storedJobIds.length > 0) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
             setAppliedJobs(jobsApplied);
+            setDisplayFilteredJobs(jobsApplied);
         }
     }, [])
     return (
@@ -23,15 +37,15 @@ const AppliedJobs = () => {
                 <div className="dropdown">
                     <label tabIndex={0} className="btn m-1">Filter by <BsChevronDown></BsChevronDown></label>
                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>All</a></li>
-                        <li><a>Remote</a></li>
-                        <li><a>Onsite</a></li>
+                        <li onClick={() => handleJobFilter('all')}><a>All</a></li>
+                        <li onClick={() => handleJobFilter('remote')}><a>Remote</a></li>
+                        <li onClick={() => handleJobFilter('onsite')}><a>Onsite</a></li>
                     </ul>
                 </div>
             </div>
             <div>
                 {
-                    (appliedJobs.length === 0) ? <p className="text-center my-8 text-gray-400 border-b-2 border-b-gray-200 py-8">You haven't applied any job yet!</p> : appliedJobs.map(application => <AppliedJob key={application.id} application={application}></AppliedJob>)
+                    (appliedJobs.length === 0) ? <p className="text-center my-8 text-gray-400 border-b-2 border-b-gray-200 py-8">You haven't applied any job yet!</p> : displayFilteredJobs.map(application => <AppliedJob key={application.id} application={application}></AppliedJob>)
                 }
             </div>
         </div>
