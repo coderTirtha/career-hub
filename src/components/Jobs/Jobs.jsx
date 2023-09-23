f863fc3b82c02a56d4576bf71f7174ed4a79f01a
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2023-09-23 11:18:32 +0600

    job section responsived

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -20,7 +20,7 @@ const Jobs = () => {
             <div className="bg-gray-50 py-12">
                 <h1 className="text-5xl font-bold text-center p-6">Total Available Jobs: {availableJobs.length}</h1>
             </div>
-            <div className="grid grid-cols-2 gap-4 my-8">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-8">
                 {
                     availableJobs.map(application => <AvailableJob key={application.id} application={application}></AvailableJob>)
                 }
